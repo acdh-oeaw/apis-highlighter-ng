fcf7341e58fab9af73d7059692d6407318150722
Birger Schacht <birger.schacht@oeaw.ac.at>
2023-10-11 09:12:48 +0200

    refactor: enhance templatetag
    This enhances the `highlight_text` templatetag to provide more of the
    setup the highlighter needs. It now creates the html div and the needed
    `data` attributes of that div itself. It also sets a `data-source`
    attribute for the javascript to know where to find an updated version of
    the text.
    
    Closes: #4

--- apis_highlighter/templatetags/apis_highlighter.py
+++ apis_highlighter/templatetags/apis_highlighter.py
@@ -1,19 +1,31 @@
 from django import template
 from django.contrib.contenttypes.models import ContentType
 from django.utils.safestring import mark_safe
+from django.urls import reverse
 from apis_highlighter.models import Annotation
 
 register = template.Library()
 
 
 @register.filter()
-def highlight_text(obj, project_id=None):
+def highlight_text(obj, fieldname="text", project_id=None):
     ct = ContentType.objects.get_for_model(obj)
     annotations = Annotation.objects.filter(text_content_type=ct, text_object_id=obj.id)
+    args = [ct.id, obj.id]
     if project_id:
         annotations = annotations.filter(project__id=project_id)
+        args.append(project_id)
 
-    text = obj.text
+    annotations_url = reverse("apis_highlighter:annotations", args=args)
+    prefix = (
+        f"<div class='highlight-text'"
+        f" data-text_object_id='{obj.id}'"
+        f" data-text_content_type_id='{ct.id}'"
+        f" data-source='{annotations_url}'>"
+    )
+    suffix = "</div>"
+
+    text = getattr(obj, fieldname)
     for ann in annotations.order_by("-start"):
         text = text[: ann.end] + "</mark>" + text[ann.end :]
         text = (
@@ -24,10 +36,5 @@ def highlight_text(obj, project_id=None):
             + f"data-hl-text-id='{ann.content_object}'>"
             + text[ann.start :]
         )
-    return mark_safe(text)
 
-
-@register.filter()
-def content_type_id(obj):
-    ct = ContentType.objects.get_for_model(obj)
-    return ct.id
+    return mark_safe(prefix + text + suffix)
